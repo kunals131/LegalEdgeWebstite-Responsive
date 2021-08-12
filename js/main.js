2105cc5c8a7b01aba863b302b4a721d355dfea76
unknown <kunalsangtiani03@gmail.com>
2021-08-12 12:34:29 +0530

    first commit

---
+++ js/main.js
@@ -0,0 +1,40 @@
+function initMap() {
+
+    //js object with latitude and logitude
+    const loc = {lat: 22.718670, lng: 75.855713}
+
+    const map = new google.maps.Map(document.querySelector('.map'), {
+        zoom: 14,
+        center : loc
+    });
+
+    const marker = new google.maps.Marker({
+        position:loc,map : map
+    });
+}
+
+//Smooth Scrolling
+
+$('#navbar a, .btn').on('click', function(event){
+
+    if (this.hash !== '') {
+        event.preventDefault();
+        const hash = this.hash;
+
+        $('html, body').animate(
+            {
+                scrollTop: $(hash).offset().top -100
+            },
+            800
+        );
+    }
+}); 
+
+document.addEventListener('scroll', function() {
+    if (window.scrollY >150) {
+        document.querySelector('#navbar').style.opacity = 0.9;
+    }
+    else {
+        document.querySelector('#navbar').style.opacity =1;
+    }
+});
